e8439f389ff86e29c40717ed13465f7fc3347610
Zhao <zhaok@wit.edu>
2019-03-20 15:46:35 -0400

    commit #1

---
+++ account.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+	  // get the popup window
+	var modal = document.getElementById('myModal');
+
+	// open the popup window
+	var btn = document.getElementById('myBtn1');
+
+	//get close
+	var close = document.getElementById('close');
+
+	//if button click, open the popup
+	btn.onclick = function(){
+		modal.style.display="flex";
+	}
+
+	//if close button clicked, close it
+	close.onclick = function(){
+		modal.style.display = "none";
+	}
+
+	//when the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event){
+		if(event.target == modal){
+			modal.styple.display = "none";
+		}
+	}
+
+}
